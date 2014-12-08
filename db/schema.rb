66d50f166d42f5290d95a0f092884cd06bb2470d
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-12-08 16:18:39 -0500

    Created friendship model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208202240) do
+ActiveRecord::Schema.define(version: 20141208211608) do
+
+  create_table "friendships", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.boolean  "confirm"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "first_name",                      null: false
