877a469553ca40ef8b636dcd084df3d89df8cc3a
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-12-08 16:12:59 -0500

    Created mailer, filled in users controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,46 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Success! Welcome to Popr!"
+    else
+      render 'new', notice: "Errors signing up, try again!"
+    end
   end
 
   def destroy
+    @user = current_user
+    @user.destroy
+    redirect_to root_path, notice: "User deleted :("
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user), notice: "Changes successful."
+    else
+      render 'edit', notice: "Error, try again!"
+    end
   end
 
   def index
+    @users = User.all
   end
 
   def show
+    @user = current_user
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar)
   end
 end
