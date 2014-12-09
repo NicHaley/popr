d1939a7b2c906462e4e2c3b0cfec15fdd2476b6c
Abdullah <abd00s@users.noreply.github.com>
2014-12-09 16:22:46 -0500

    Fixed friendship confirmation bug

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,7 +2,6 @@ class FriendshipsController < ApplicationController
 	def create
 		@friendship = current_user.friendships.build(friend_id: params[:friend_id])
 		if Friendship.exists?(friend_id: current_user.id, user_id: params[:friend_id])
-			# @friendship2 = Friendship.find(params[:friend_id])
 			@friendship2 = Friendship.where(friend_id: current_user.id, user_id: params[:friend_id]).first
 		end
 		if @friendship.save
