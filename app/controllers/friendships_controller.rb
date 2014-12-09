0f41704e2dea62c907ad25c82a3df526b5dc5612
Abdullah <abd00s@users.noreply.github.com>
2014-12-09 16:21:58 -0500

    Fixed friendship confirmation bug

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,19 +1,19 @@
 class FriendshipsController < ApplicationController
 	def create
 		@friendship = current_user.friendships.build(friend_id: params[:friend_id])
-		# if Friendship.exists?(friend_id: params[:friend_id], user_id: current_user.id)
-		# 	# @friendship2 = Friendship.find(params[:friend_id])
-		# 	@friendship2 = Friendship.where(friend_id: params[:friend_id], user_id: current_user.id).first
-		# end
+		if Friendship.exists?(friend_id: current_user.id, user_id: params[:friend_id])
+			# @friendship2 = Friendship.find(params[:friend_id])
+			@friendship2 = Friendship.where(friend_id: current_user.id, user_id: params[:friend_id]).first
+		end
 		if @friendship.save
-			# if @friendship.confirmed?
-			# 	@friendship.confirm = true
-			# 	@friendship.save
-			# 	if @friendship2
-			# 		@friendship2.confirm = true
-			# 		@friendship2.save
-			# 	end
-			# end
+			if @friendship.confirmed?
+				@friendship.confirm = true
+				@friendship.save
+				if @friendship2
+					@friendship2.confirm = true
+					@friendship2.save
+				end
+			end
 			flash[:notice] = "Added friend"
 			redirect_to root_url
 		else
