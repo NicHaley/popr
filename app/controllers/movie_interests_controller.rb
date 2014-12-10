d85b2b1cc7f7bee52c1ec95ecd2a6993338aebbd
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-12-10 14:15:36 -0500

    Filled in MovieInterests controller

--- app/controllers/movie_interests_controller.rb
+++ app/controllers/movie_interests_controller.rb
@@ -1,22 +1,48 @@
 class MovieInterestsController < ApplicationController
-  def create
-  end
+  before_filter :set_movie_interest, only: [:show, :edit, :update, :destroy]
 
-  def destroy
+  def index
+    @movie_interests = MovieInterests.where(user_id: current_user.id)
   end
-
+  
   def show
   end
 
   def new
+    @movie_interest = MovieInterest.new
+  end
+
+  def create
+    @movie_interest = MovieInterest.new(movie_interest_params)
+    @user = User.find(params[:user_id])
+    if @movie_interest.save
+      redirect_to user_movie_interests_path(@user), notice: "Movie interest created!"
+    else
+      render 'new', notice: 'Error creating movie interest'
   end
 
   def edit
   end
 
   def update
+    if @movie_interest.update_attributes(movie_interest_params)
+      redirect_to user_movie_interests_path(@user), notice: "Movie interest updated!"
+    else
+      render 'edit', notice: "Error, try again!"
+    end
+  end
+  
+  def destroy
+    @movie_interest.destroy
+     redirect_to user_movie_interests_path(@user), notice: "Movie interest succesfully deleted."
   end
 
-  def index
+  private
+  def movie_interest_params
+    params.require(:movie_interest).permit(:rt_id, :event_id, :user_id, :watched?, :wished?, :user_score)
+  end
+
+   def set_movie_interest
+    @movie_interest = MovieInterest.find(params[:id])
   end
 end
