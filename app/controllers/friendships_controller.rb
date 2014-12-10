3f6cec42fe8faea67721f9e718779dec807b1e31
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-12-09 21:41:27 -0500

    Added comments to friendship controller and model

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,14 +1,21 @@
 class FriendshipsController < ApplicationController
 	def create
+		# Create a new friendship user_id of current_user, and friend_id of params (selected in index.html.erb)
 		@friendship = current_user.friendships.build(friend_id: params[:friend_id])
+
+		# Check if the friend has created a friendship as well. A mutual friendship is created when the first user's... 
+		# ID matches the friend_id of the second user, and vice verca. Ex. Friendship if: #<Friendship id: 1, user_id: 2, friend_id: 1>
+		# AND #<Friendship id: 2, user_id: 1, friend_id: 2>
 		if Friendship.exists?(friend_id: current_user.id, user_id: params[:friend_id])
 			@friendship2 = Friendship.where(friend_id: current_user.id, user_id: params[:friend_id]).first
 		end
 		if @friendship.save
+
+			# Confirm using defined friendship method if both users have befriended each other
 			if @friendship.confirmed?
 				@friendship.confirm = true
 				@friendship.save
-				if @friendship2
+				if @friendship2			#I don't think we need this nested if statement
 					@friendship2.confirm = true
 					@friendship2.save
 				end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,6 +2,7 @@ class Friendship < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :friend, :class_name => "User"
 
+	# Returns true if the user has the friend_id, and the friend has the user_id
 	def confirmed?
 		User.find(user_id).friends.include?(User.find(friend_id)) && User.find(friend_id).friends.include?(User.find(user_id)) 
 	end
