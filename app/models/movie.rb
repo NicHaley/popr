179030810567f17a9ad077c3122d5f31292cd45e
Abdullah <abd00s@users.noreply.github.com>
2014-12-12 11:10:04 -0500

    Added comments clarifying the HTTParty interface with the RT API

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,6 +5,9 @@ class Movie
   attr_accessor :title, :year, :poster, :synopsis, :consensus, :rating, :cast
 
   def initialize(options)
+    # The initialize() method stores insatnce variables of the returned JSON object 
+    # values. We only assign values we want to retrieve/ display. This allows us to 
+    # call @instance.[attribute] methods on our movies. 
     @title = options['title']
     @year = options['year']
     @poster = options['posters']['original'].gsub("tmb", "det")
@@ -15,8 +18,12 @@ class Movie
   end
 
   def self.find_movie(movie_id)
+    # The .find_movie function is called on the Movie class in the EventsController
+    # show action, and is passed that specific event's rt_id. We pass the response 
+    # into JSON.parse() as the Rotten Tomatoes API returns a string.
     response = JSON.parse(get("/movies/#{movie_id}.json?apikey=5z3dxvtjcr5nxmqefqnbh25z"))
-
+    # We then instantiate an instance of the Movie class passing it the response.
+    # This calls the initialize() method.
     Movie.new(response)
   end
 end
