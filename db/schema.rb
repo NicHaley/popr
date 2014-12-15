7b0b2b323509d737a3f2d04cfa7ada5c680c3d67
Abdullah <abd00s@users.noreply.github.com>
2014-12-15 13:55:01 -0500

    Generate Comment model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141214175011) do
+ActiveRecord::Schema.define(version: 20141215185243) do
+
+  create_table "comments", force: true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "commitments", force: true do |t|
     t.integer  "event_id"
