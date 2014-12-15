50208eb057d88bc72b3a0417532d414d9377b078
Abdullah <abd00s@users.noreply.github.com>
2014-12-15 18:17:04 -0500

    Uncommented JS stuff I ignored previously

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,94 +1,94 @@
-// window.myMap = {};
+window.myMap = {};
 
-// var map;
-
-
-// $(document).on('ready page:load', function() {
-
-// 	if ("geolocation" in navigator) {
-// 		//Initialize the map on page load
-// 		myMap.init();
-
-// 		// Retrieve the coords of all events
-// 		var coords = $('#map-canvas').data('coords');
-
-// 		if (coords){
-// 			myMap.addMarkers(coords);
-// 		}
-
-// 	}
-// });
-
-// //Initialize Map
-// myMap.init = function() {
-// 	if(navigator.geolocation){
+var map;
 
 
-// 	//Set the default map settings
-// 	var mapOptions = {
-// 		zoom: 14,
-// 		mapTypeId: google.maps.MapTypeId.ROADMAP
-// 	};
+$(document).on('ready page:load', function() {
 
-// 	//Set map inside the #map-canvas
-// 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	if ("geolocation" in navigator) {
+		//Initialize the map on page load
+		myMap.init();
 
-// 	//Set the current position into map
-// 	navigator.geolocation.getCurrentPosition(function(position){
+		// Retrieve the coords of all events
+		var coords = $('#map-canvas').data('coords');
 
-// 		var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-// 		var infoWindow = new google.maps.InfoWindow({
-// 			map: map,
-// 			position: pos
-// 		});
-// 		//Add marker for current position
-// 		var marker = new google.maps.Marker({
-// 			position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-// 			map: map
-// 		});
+		if (coords){
+			myMap.addMarkers(coords);
+		}
 
-// 		map.setCenter(pos);
+	}
+});
 
-// 		var latitude = position.coords.latitude;
-// 		var longitude = position.coords.longitude;	
-
-// 		$.ajax({
-// 			url:"/all_events",
-// 			method: "GET",
-// 			data: {
-// 				latitude: latitude,
-// 				longitude: longitude
-// 			},
-// 			dataType: 'script'
-// 		});
-
-// 	});
-
-// 	} else {
-// 	  	//Browser does not support geolocation
-// 	  	document.getElementById('map-canvas').innerHTML = 'No Geolocation Support.';
-// 	}
-
-// };
+//Initialize Map
+myMap.init = function() {
+	if(navigator.geolocation){
 
 
-// //Add markers for other events
-// myMap.addMarkers = function(coords){
+	//Set the default map settings
+	var mapOptions = {
+		zoom: 14,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
 
-// 	var image = "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"
+	//Set map inside the #map-canvas
+	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-// 	coords.forEach(function(coord){
-// 		var myMarker = new google.maps.Marker({
-// 			position: new google.maps.LatLng(coord.latitude, coord.longitude),
-// 			map: map,
-// 			icon: image
-// 		});
-// 	});
-// }
+	//Set the current position into map
+	navigator.geolocation.getCurrentPosition(function(position){
+
+		var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+		var infoWindow = new google.maps.InfoWindow({
+			map: map,
+			position: pos
+		});
+		//Add marker for current position
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+			map: map
+		});
+
+		map.setCenter(pos);
+
+		var latitude = position.coords.latitude;
+		var longitude = position.coords.longitude;	
+
+		$.ajax({
+			url:"/all_events",
+			method: "GET",
+			data: {
+				latitude: latitude,
+				longitude: longitude
+			},
+			dataType: 'script'
+		});
+
+	});
+
+	} else {
+	  	//Browser does not support geolocation
+	  	document.getElementById('map-canvas').innerHTML = 'No Geolocation Support.';
+	}
+
+};
 
 
+//Add markers for other events
+myMap.addMarkers = function(coords){
+
+	var image = "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"
+
+	coords.forEach(function(coord){
+		var myMarker = new google.maps.Marker({
+			position: new google.maps.LatLng(coord.latitude, coord.longitude),
+			map: map,
+			icon: image
+		});
+	});
+}
 
 
 
 
-//   
+
+
+  
