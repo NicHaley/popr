1f174b2bb67496d676c7854afcfc6850379a9ad9
wduntak <wduntak@gmail.com>
2014-12-17 15:02:43 -0500

    Removed path in application.js

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -39,19 +39,16 @@ myMap.init = function() {
 				var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 				
 
-				  var contentString = '<div id="content">'+
+				  var contentString = '<div id="info-window-content">'+
 					'<div id="siteNotice">'+
 					'</div>'+
-					'<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+					'<h3 id="firstHeading" class="firstHeading">Uluru</h3>'+
 					'<div id="bodyContent">'+
 					'<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
 					'sandstone rock formation in the southern part of the '+
 					'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
 					'south west of the nearest large town, Alice Springs; 450&#160;km '+
-					'(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-					'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-					'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-					'Aboriginal people of the area. It has many springs, waterholes, '+
+					'(280&#160;mi) by road.'+
 					'rock caves and ancient paintings. Uluru is listed as a World '+
 					'Heritage Site.</p>'+
 					'<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
