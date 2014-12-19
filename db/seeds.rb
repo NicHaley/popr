8f94f0bb9ea45713e38a9dd45d1d5c0690930385
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-12-19 15:33:01 -0500

    Added additonal seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 users = User.create([{first_name: "Joe", last_name: "Smith", email: "example@email.com", password: "123", password_confirmation: "123", avatar: "123"},
+	{first_name: "George", last_name: "Bush", email: "example3@email.com", password: "123", password_confirmation: "123", avatar: "123"},
+	{first_name: "Julie", last_name: "Hache", email: "example4@email.com", password: "123", password_confirmation: "123", avatar: "123"},
+	{first_name: "Nicholas", last_name: "Haley", email: "example5@email.com", password: "123", password_confirmation: "123", avatar: "123"},
+	{first_name: "Abdullah", last_name: "Pharaon", email: "example6@email.com", password: "123", password_confirmation: "123", avatar: "123"},
+	{first_name: "Wangdu", last_name: "Duntak", email: "example7@email.com", password: "123", password_confirmation: "123", avatar: "123"},
   {first_name: "Jane", last_name: "Doe", email: "example2@email.com", password: "123", password_confirmation: "123", avatar: "123"}])
