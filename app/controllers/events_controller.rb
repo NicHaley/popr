f98455641df29c6a5102c67a3c6f56a3e249dc6a
wduntak <wduntak@gmail.com>
2015-01-02 15:36:28 -0500

    Passed a constraint on marker to show only events not passed show date

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -29,7 +29,7 @@ class EventsController < ApplicationController
 
     @current_position = Event.near(c_position, 10, units: :km)
 
-    @friend_check = @current_position.select{|event| event.host.is_friend?(current_user)}
+    @friend_check = @current_position.select{|event| event.host.is_friend?(current_user) && event.not_passed}
     @nearby_coords = @friend_check.map {|event| {latitude: event.latitude.to_f, longitude: event.longitude.to_f}}
 
     respond_to do |format|
