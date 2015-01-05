b647ba2151b3fcd1ba895cc753c5e93b51562e4b
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-01-05 16:19:16 -0500

    Added bio and hometown fields to user edit and show pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -83,6 +83,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar, :bio, :hometown)
   end
 end
