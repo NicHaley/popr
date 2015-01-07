627dcdecfc047fceb33712ff14a9b7bb6acf0f23
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-01-07 10:47:49 -0500

    Fixed movie_interest validation bug

--- app/models/movie_interest.rb
+++ app/models/movie_interest.rb
@@ -2,6 +2,7 @@ class MovieInterest < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
 
-  # validates :rt_id, presence: true, uniqueness: {message: "Already wished!"}
+  validates :rt_id, presence: true, uniqueness: { scope: :user_id,
+    message: "Already wished!" }
   
 end
