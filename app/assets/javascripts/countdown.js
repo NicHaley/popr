dbdbeb51486d9748857df57e10af9d0699dc9dd3
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-01-08 11:36:56 -0500

    Implemented character coundown for movie reviews

--- app/assets/javascripts/countdown.js
+++ app/assets/javascripts/countdown.js
@@ -1,11 +1,11 @@
 $(document).ready(function($) {
-    updateCountdown();
-    $('.message').change(updateCountdown);
-    $('.message').keyup(updateCountdown);
+	$('.countdown').text('150 characters remaining.')
+	$('.message').change(updateCountdown);
+	$('.message').keyup(updateCountdown);
 });
 
 function updateCountdown() {
-    // 140 is the max message length
-    var remaining = 150 - $('.message').val().length;
-    $('.countdown').text(remaining + ' characters remaining.');
+  // 140 is the max message length
+  var remaining = 150 - $('.message').val().length;
+  $('.countdown').text(remaining + ' characters remaining.');
 }
