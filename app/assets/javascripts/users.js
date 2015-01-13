933d300e4f795f745318147e9b610de4d9933a5b
Abdullah <abd00s@users.noreply.github.com>
2015-01-13 13:56:07 -0500

    Fixed friend search bugs and added a back to all friends button

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,8 +1,24 @@
+// $(document).on('ready page:change', (function() {
 $(document).ready(function() {
+// $(document).on("page:change", function() {
   $('#search-form').submit(function(event) {
     event.preventDefault();
     var searchValue = $('#friend_search').val();
 
     $.getScript('/users/' + gon.user_id + '?friend_search=' + searchValue);
   });
+});
+
+$(document).ajaxSuccess(function() {
+   $('#search-form').submit(function(event) {
+    event.preventDefault();
+    var searchValue = $('#friend_search').val();
+
+    $.getScript('/users/' + gon.user_id + '?friend_search=' + searchValue);
+  }); 
+
+  $('#back-to-friends').click(function(event) {
+    event.preventDefault();
+    $.getScript('/users/' + gon.user_id);
+  });  
 });
