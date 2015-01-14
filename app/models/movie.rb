3e4159794ac54834ef435a58542a4ebf833bbdb0
Abdullah <abd00s@users.noreply.github.com>
2015-01-14 16:17:04 -0500

    API hack

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,6 @@
 class Movie
   include HTTParty
   base_uri 'api.rottentomatoes.com/api/public/v1.0'
-
   attr_accessor :title, :year, :poster, :synopsis, :consensus, :rating, :cast
 
   def initialize(options)
@@ -18,10 +17,20 @@ class Movie
   end
 
   def self.find_movie(movie_id)
+    api_key = ["5z3dxvtjcr5nxmqefqnbh25z", "asufuw64uzqn4pz7pb2v2kkt"]
+    key = 0
+    rotten_api = ""
+    if key == 0
+      rotten_api = api_key[0]
+      key = 1
+    else
+      rotten_api = api_key[1]
+      key = 0
+    end
     # The .find_movie function is called on the Movie class in the EventsController
     # show action, and is passed that specific event's rt_id. We pass the response 
     # into JSON.parse() as the Rotten Tomatoes API returns a string.
-    response = JSON.parse(get("/movies/#{movie_id}.json?apikey=5z3dxvtjcr5nxmqefqnbh25z"))
+    response = JSON.parse(get("/movies/#{movie_id}.json?apikey=" + rotten_api.to_s))
     # We then instantiate an instance of the Movie class passing it the response.
     # This calls the initialize() method.
     Movie.new(response)
