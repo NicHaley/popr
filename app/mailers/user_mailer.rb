969b2571f6dcecc25bd9a612d6d684d8b1c69b35
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-01-14 17:12:23 -0500

    Styled activation emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,13 +4,13 @@ class UserMailer < ActionMailer::Base
 	def activation_needed_email(user)
 		@user = user
 		@url  = "http://localhost:3000/users/#{@user.activation_token}/activate"
-		mail(to: @user.email, subject: 'Sample Email')
+		mail(to: @user.email, subject: 'Activate Your Popr Account')
 	end
 
 	def activation_success_email(user)
 		@user = user
 		@url  = "http://localhost:3000/"
-		mail(to: @user.email, subject: 'Your account is now activated')
+		mail(to: @user.email, subject: 'Popr Account Activated')
 	end
 
 end
