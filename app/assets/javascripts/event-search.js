7a38da1d84f9e6332b5a2ecfcd877c6f1bf59366
Abdullah <abd00s@users.noreply.github.com>
2015-01-14 13:35:42 -0500

    Added near me button

--- app/assets/javascripts/event-search.js
+++ app/assets/javascripts/event-search.js
@@ -4,6 +4,17 @@ $(document).ready(function() {
     var searchValue = $('#search_location').val();
 
     $.getScript('/all_events/?search_location=' + searchValue);
+    $('#search_location').val("");
   });
 
+  $('#near-me-button').click(function(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(function(position){
+      var latitude = position.coords.latitude;
+      var longitude = position.coords.longitude;  
+    $.getScript('/all_events?latitude=' + latitude + '&longitude=' + longitude);
+    });
+
+    // ?latitude=43.647031299999995&longitude=-79.3872371
+  });
 });
