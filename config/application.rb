15637ab1c483d2e6a756f7f673b3fc6dbc130438
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-01-16 17:57:29 -0500

    Updated application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,16 +11,8 @@ module Popr
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # config.serve_static_assets = true
-    # config.assets.precompile += [
-    #   'application.scss.css',
-    #   'base.css.scss',
-    #   'foundation_and_overrides.scss',
-    #   'layout.css.scss',
-    #   'responsive-tables.css.scss',
-    #   'starRating.css.scss',
-    #   'staticStars.css.scss'
-    # ]
+    config.serve_static_assets = true
+    config.assets.precompile += %w( *.css *.js )
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
