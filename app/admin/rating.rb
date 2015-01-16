4f0889019c854bfe9374717a1bb442067d49934f
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-01-16 14:17:24 -0500

    Added a user admin, minor styling updates to movie title

---
+++ app/admin/rating.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Rating do
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  permit_params :user_id, :user_score, :watched, :review, :rt_id, :actors, :directors, :genres
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+
+end
