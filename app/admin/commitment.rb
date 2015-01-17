63072f235078e6045ade700c28e8ec25fa35a7e5
Abdullah <abd00s@users.noreply.github.com>
2015-01-16 21:22:14 -0500

    Added editable attributes of commitment

--- app/admin/commitment.rb
+++ app/admin/commitment.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Commitment do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :party_size
+  permit_params :party_size, :event_id, :user_id
   #
   # or
   #
