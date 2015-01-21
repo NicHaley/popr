c06ded8b03c18a1448bf5bf3e6e1a0bba1283696
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-01-21 15:16:05 -0500

    Made AWS keys upercase

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['aws_secret'],                        # required
-    :aws_secret_access_key  => ENV['aws_secret_access_key'],                        # required
+    :aws_access_key_id      => ENV['AWS_SECRET'],                        # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                        # required
     :region                 => 'us-west-2',                  # optional, defaults to 'us-east-1'
     # :host                   => 's3.example.com',             # optional, defaults to nil
     # :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
